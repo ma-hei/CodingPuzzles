f6a562d49eb8f62282b7caa8967dc34680f970f6
ma-hei <marten_heidemeyer@web.de>
2017-05-04 23:19:20 -0700

    almost...

--- coding/codeforces/bit_palindrome.cpp
+++ coding/codeforces/bit_palindrome.cpp
@@ -10,20 +10,15 @@ int main(){
 	int temp = 0;
 	int count = 0;
 	while(temp<n){
-		cout<<"temp: "<<temp<<"\n";
 		if (blocksize*10<=n){
-			//count+= blocksize<=10 ? 9 : blocksize;
+			count+= 9 * (blocksize==1 ? 1 : blocksize/10);
 			temp=blocksize*10;
 			blocksize*=10;
 		} else if (temp+blocksize<=n){
 			temp+=blocksize;
 			count+=blocksize==1 ? 1 : blocksize/10;
 		} else {
-			cout<<"left: "<<(n-temp)<<"\n";
-			//n = 6340 -> blocksize = 1000
-			//n = 154101 -> blocksize = 100000
-			int temp_ = n/blocksize;
-			count+= (n-temp-temp_)/10;
+			count+= (n-temp-(n/blocksize))>0 ? (n-temp-(n/blocksize))/10+1: 0;
 			break;
 		}
 	}
