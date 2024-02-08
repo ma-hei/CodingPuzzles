df82f680d3fe4d9b6fa640f13453be36c145ec63
ma-hei <mheideme@gmail.com>
2024-02-07 17:58:58 -0800

    here we go again..

---
+++ codeforces/did_we_get_everything_covered.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    int t;
+    cin >> t;
+    for (int t_=0; t_<t; t_++) {
+        int n, k, m;
+        cin >> n >> k >> m;
+        string s;
+        cin >> s;
+    }
+}
